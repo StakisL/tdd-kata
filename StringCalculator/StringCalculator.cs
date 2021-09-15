525e363d1769bbac852b860adfb64f03f5ccb398
stanislav <s.lebel@dodopizza.com>
2021-09-15 17:36:28 +0300

    Add test for custom delimiters

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace StringCalculator
 {
     public class StringCalculator
@@ -6,10 +8,35 @@ namespace StringCalculator
         {
             if (string.IsNullOrEmpty(input))
                 return 0;
-            
+
+            return input.StartsWith("//") ? AddWithCustomDelimiter(input) : AddWithoutDelimiter(input);
+        }
+
+        private int AddWithoutDelimiter(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return 0;
+
             var sum = 0;
             // ReSharper disable once PossiblyMistakenUseOfParamsMethod
             var numbers = input.Split(',', '\n');
+            for (int i = 0; i <numbers.Length; i++)
+            {
+                sum += int.Parse(numbers[i]);
+            }
+
+            return sum;
+        }
+
+        private int AddWithCustomDelimiter(string input)
+        {
+            var result = input.Substring(2, 1);
+            var numberDigits = input.IndexOf('\n');
+            var numbersString = input.Substring(numberDigits + 1);
+            
+            var sum = 0;
+            // ReSharper disable once PossiblyMistakenUseOfParamsMethod
+            var numbers = numbersString.Split(',', '\n', Convert.ToChar(result));
             for (int i = 0; i <numbers.Length; i++)
             {
                 sum += int.Parse(numbers[i]);
