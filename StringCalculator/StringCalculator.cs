d32507514b791b6306379c3cf1081aa302004c0b
stanislav <s.lebel@dodopizza.com>
2021-09-15 17:17:14 +0300

    Add different dilimiters support

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -4,8 +4,12 @@ namespace StringCalculator
     {
         public int Add(string input)
         {
+            if (string.IsNullOrEmpty(input))
+                return 0;
+            
             var sum = 0;
-            var numbers = input.Split(',');
+            // ReSharper disable once PossiblyMistakenUseOfParamsMethod
+            var numbers = input.Split(',', '\n');
             for (int i = 0; i <numbers.Length; i++)
             {
                 sum += int.Parse(numbers[i]);
