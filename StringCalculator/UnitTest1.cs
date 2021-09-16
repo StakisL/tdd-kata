c32320f31d274b98516eddb93cf011df8ead30a2
stanislav <s.lebel@dodopizza.com>
2021-09-16 17:09:55 +0300

    Add test when negative number passed

---
+++ StringCalculator/UnitTest1.cs
@@ -0,0 +1,48 @@
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace StringCalculator
+{
+    public class Tests
+    {            
+        StringCalculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new StringCalculator();
+        }
+
+        [Test]
+        [TestCase("1,2", 3)]
+        [TestCase("", 0)]
+        [TestCase("1",1)]
+        [TestCase("1,2,3",6)]
+        [TestCase("1\n2,3",6)]
+        [TestCase("//;\n1;2",3)]
+        public void ShouldReturnThree_WhenOneAndTwoPassed(string input, int expected)
+        {
+            int result = _calculator.Add(input);
+
+            result.Should().Be(expected);
+        }
+
+        [Test]
+        [TestCase("-1,2,3", "-1")]
+        [TestCase("-1,-22,3", "-1,-22")]
+        [TestCase("-5,-6,-7", "-5,-6,-7")]
+        public void Add_ShouldThrowException_WhenNegativeNumberPassed(string input, string expectedResult)
+        {
+            try
+            {
+                _calculator.Add(input);
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual(e.Message, $"Negatives not allowed: {expectedResult}");
+            }
+        }
+    }
+}
