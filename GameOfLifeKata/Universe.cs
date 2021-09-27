4701ad3aa438214f39ea9ddd3d463f1240ec999b
stanislav <s.lebel@dodopizza.com>
2021-09-27 17:43:31 +0300

    Add check for gneration

--- GameOfLifeKata/Universe.cs
+++ GameOfLifeKata/Universe.cs
@@ -6,19 +6,35 @@ namespace GameOfLifeKata
     {
         private readonly int _width = 10;
         private readonly int _height = 10;
+        private bool[,] _generation;
         
         public Universe(){}
         public Universe(int width, int height)
         {
             _width = width;
             _height = height;
+            _generation = new bool[_width, _height];
         }
 
         public int GetField() => _width * _height;
 
         public void SetFirstGeneration(bool[,] generation)
         {
-            throw new ArgumentException();
+            if (!IsGenerationAlive(generation))
+            {
+                throw new ArgumentException();
+            }
+
+            _generation = generation;
+        }
+
+        private static bool IsGenerationAlive(bool[,] generation)
+        {
+            for (var i = 0; i < generation.GetLength(0); i++)
+                for (var j = 0; j < generation.GetLength(1); j++)
+                    if (generation[i, j])
+                        return true;
+            return false;
         }
     }
 }
